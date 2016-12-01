b29ccfa6ffb897542f0c178b8e7a31c18551f1eb
matthewsidneyjacobs <matthewsidneyjacobs@gmail.com>
2016-12-01 14:39:33 -0700

    working file

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,4 @@
-/*var gulp = require('gulp'),
+var gulp = require('gulp'),
     del = require('del'),
     sass = require('gulp-sass'),
     sourcemaps = require('gulp-sourcemaps'),
@@ -7,50 +7,55 @@
     print = require('gulp-print'),
     babel = require('gulp-babel');
     //babel-preset-es2015
-    */
 
-var gulp = require('gulp'),
-    sass = require('gulp-sass'),
-    sourcemaps = require('gulp-sourcemaps');
 
 var CacheBuster = require('gulp-cachebust');
 var cachebust = new CacheBuster();
 
+gulp.task('sayhello', function() {
+  console.log('hello dm16')
+});
 
-// gulp.task('clean', function (cb) {
-//     del([
-//         'dist'
-//     ], cb);
-// });
+gulp.task('saybye', function() {
+  console.log('bye dm16')
+});
 
-// gulp.task('build-css', ['clean'], function () {
-//     return gulp.src('./styles/*')
-//         .pipe(sourcemaps.init())
-//         .pipe(sass())
-//         .pipe(cachebust.resources())
-//         .pipe(concat('styles.css'))
-//         .pipe(sourcemaps.write('./maps'))
-//         .pipe(gulp.dest('./dist'));
-// });
+gulp.task('clean', function (cb) {
+    del([
+        'dist'
+    ], cb);
+});
+
+gulp.task('build-css', function() {
+  gulp.src('./styles/*')
+  .pipe(sourcemaps.init())
+  .pipe(sass())
+  .pipe(cachebust.resources())
+  .pipe(concat('styles.css'))
+  .pipe(sourcemaps.write('./maps'))
+  .pipe(gulp.dest('./dist'));
+});
+
+// you can pass in an array of multiple tasks
+gulp.task('build', ['build-js','build-css'], function() {
+  return gulp.src('index.html')
+    .pipe(cachebust.references())
+    .pipe(gulp.dest('dist'));
+})
 
 
-// gulp.task('build-js', ['clean'], function() {
-//    return gulp.src('js/**/*.js')               
-    //   .pipe(sourcemaps.init())
-    //   .pipe(print())                        
-    //   .pipe(babel({ presets: ['es2015'] }))
-    //   .pipe(concat('bundle.js'))
-    //   //.pipe(uglify())
-    //   .pipe(sourcemaps.write('./')) 
-    //   .pipe(gulp.dest('./dist/js')); 
-// });
+// gulp watch first array is something for gulp to watch, second array is tasks to run
+gulp.task('watch', function() {
+    return gulp.watch(['./index.html','./partials/*.html', './styles/*.*css', './js/**/*.js'], ['build']);
+});
 
-// gulp.task('build', [ 'clean', 'build-css', 'build-js'], function() {
-//     return gulp.src('index.html')
-//         .pipe(cachebust.references())
-//         .pipe(gulp.dest('dist'));
-// });
-
-// gulp.task('watch', function() {
-//     return gulp.watch(['./index.html','./partials/*.html', './styles/*.*css', './js/**/*.js'], ['build']);
-// });
+gulp.task('build-js', function() {
+   return gulp.src('js/**/*.js')
+      .pipe(sourcemaps.init())
+      .pipe(print())
+      .pipe(babel({ presets: ['es2015'] }))
+      .pipe(concat('bundle.js'))
+      // .pipe(uglify())
+      .pipe(sourcemaps.write('./'))
+      .pipe(gulp.dest('./dist/js'));
+});
--- js/controller.js
+++ js/controller.js
@@ -1,7 +1,12 @@
 angular.module("app").controller("ctrl", function($scope, service){
-   
+
     $scope.thisappisbroken = "This app is working";
 
     $scope.germData = service.getGerms();
-    
+
+    $scope.killGerm = (g) => {
+      let yay= "Yay"
+      console.log(`This worked ${yay}`)
+    };
+
 });
